e7ed8871a90595979feda224db8289d03cc92038
AsareelDon <asareeldonpena@gmail.com>
2024-08-02 09:53:02 +0800

    remove files for refractoring

--- crud/src/main/java/com/rest_api/crud/services/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.rest_api.crud.services;
-
-import com.rest_api.crud.entity.Users;
-
-public class UserService {
-}
--- crud/src/main/java/com/rest_api/crud/services/impl/UserServicesImplements.java
+++
@@ -1,9 +0,0 @@
-package com.rest_api.crud.services.impl;
-
-import com.rest_api.crud.services.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServicesImplements {
-
-}
