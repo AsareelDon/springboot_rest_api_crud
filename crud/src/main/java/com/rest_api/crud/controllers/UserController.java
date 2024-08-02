c534100c1034e8c74d91c5b9458783a70bd4c230
AsareelDon <asareeldonpena@gmail.com>
2024-08-02 16:00:45 +0800

    update controller web bind annotation for HTTP protocol requests

--- crud/src/main/java/com/rest_api/crud/controllers/UserController.java
+++ crud/src/main/java/com/rest_api/crud/controllers/UserController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -36,10 +39,12 @@ public class UserController {
         return userService.findById(id);
     }
     
+    @PostMapping
     public UserEntity createUser(@RequestBody UserEntity userEntity) {
         return userService.createUser(userEntity);
     }
 
+    @PutMapping("/{id}")
     public UserEntity updateUser(@RequestBody UserEntity userEntity) {
         return userService.updateUser(userEntity);
     }
