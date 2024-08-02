acf1af97b1420bdbc1d35f8897fa45fb92e5271f
AsareelDon <asareeldonpena@gmail.com>
2024-08-02 15:37:48 +0800

    update methods for services implements

--- crud/src/main/java/com/rest_api/crud/services/impl/UserServiceImp.java
+++ crud/src/main/java/com/rest_api/crud/services/impl/UserServiceImp.java
@@ -1,35 +1,45 @@
 package com.rest_api.crud.services.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.rest_api.crud.entity.UserEntity;
+import com.rest_api.crud.repository.UserRepository;
 import com.rest_api.crud.services.UserService;
 
 @Service
 public class UserServiceImp implements UserService {
 
+    private final UserRepository userRepository;
+
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserEntity findById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    public List<UserEntity> findAllUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserEntity> findById(Long id) {
+        return userRepository.findById(id);
     }
 
     @Override
     public UserEntity createUser(UserEntity userEntity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+        return userRepository.save(userEntity);
     }
 
     @Override
     public UserEntity updateUser(UserEntity userEntity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+        return userRepository.save(userEntity);
     }
 
     @Override
     public void deleteUser(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+        userRepository.deleteById(id);
     }
-
 }
